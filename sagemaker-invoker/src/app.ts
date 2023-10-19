ecc72602dffe07d66fb1e14c4f163f511fdd2b20
Grant Starkman <gstarkma@amazon.com>
2023-10-19 10:00:00 -0600

    lambda robustness

--- sagemaker-invoker/src/app.ts
+++ sagemaker-invoker/src/app.ts
@@ -3,6 +3,28 @@ import { SageMakerRuntimeClient, InvokeEndpointCommand } from '@aws-sdk/client-s
 
 const sagemakerClient = new SageMakerRuntimeClient({ region: 'us-west-2' });
 
+function isValidPayload(payload: string): boolean {
+    return payload.trim().length > 0;
+}
+
+function handleError(error: any): APIGatewayProxyResult {
+    console.error('Error:', error);
+    let errorMessage = 'Error invoking SageMaker Endpoint';
+
+    if (error instanceof Error) {
+        errorMessage = error.message;
+    }
+
+    if (error.code === 'ValidationError') {
+        errorMessage = 'Invalid input format';
+    }
+
+    return {
+        statusCode: 500,
+        body: JSON.stringify({ message: errorMessage }),
+    };
+}
+
 async function invokeEndpoint(libsvmData: string): Promise<APIGatewayProxyResult> {
     const params = {
         EndpointName: 'endpoint-80b8c435d12d-2023-10-19T04-13-15-686175',
@@ -14,29 +36,30 @@ async function invokeEndpoint(libsvmData: string): Promise<APIGatewayProxyResult
 
     try {
         const response = await sagemakerClient.send(command);
+        const decodedResponse = String.fromCharCode(...response.Body!);
+        const predictions = decodedResponse.split(',').map(parseFloat);
+
         console.log(response);
         return {
             statusCode: 200,
-            body: JSON.stringify(response),
+            body: JSON.stringify({ predictions }),
         };
     } catch (error) {
-        console.error('Error invoking SageMaker Endpoint:', error);
-
-        let errorMessage = 'Error invoking SageMaker Endpoint';
-        if (error instanceof Error) {
-            errorMessage = error.message;
-        }
-
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: errorMessage }),
-        };
+        return handleError(error);
     }
 }
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // Extract LIBSVM data from the incoming event body
     const libsvmData = event.body || '';
+
+    if (!isValidPayload(libsvmData)) {
+        console.error('Invalid payload:', libsvmData);
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Invalid input format' }),
+        };
+    }
 
     return await invokeEndpoint(libsvmData);
 };
