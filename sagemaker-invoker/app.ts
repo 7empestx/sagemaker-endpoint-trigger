b7d282630c6851dee79f4d0fed2bebc367a5505a
Grant Starkman <gstarkma@amazon.com>
2023-10-18 22:47:49 -0600

    adjustment to content type

--- sagemaker-invoker/app.ts
+++ sagemaker-invoker/app.ts
@@ -3,13 +3,18 @@ import { SageMakerRuntimeClient, InvokeEndpointCommand } from '@aws-sdk/client-s
 
 const sagemakerClient = new SageMakerRuntimeClient({ region: 'us-west-2' });
 
-const data = Array(51).fill(0).join(',');
+const data =
+    '0 ' +
+    Array(51)
+        .fill(0)
+        .map((v, i) => `${i + 1}:${v}`)
+        .join(' ');
 
 async function invokeEndpoint(): Promise<APIGatewayProxyResult> {
     const params = {
         EndpointName: 'endpoint-9fbb3b825b31-2023-10-18T18-45-58-847556',
         Body: data,
-        ContentType: 'text/csv',
+        ContentType: 'text/libsvm', // Change the content type
     };
 
     const command = new InvokeEndpointCommand(params);
